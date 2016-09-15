8c159fd1b67f32fdf0390270a89a98bcae431a87
vincent manzanilla <vincent.manzanilla@gmail.com>
2016-09-15 11:49:15 +0200

    compare list

---
+++ compare_list/compare_list_from_files.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+from glob import glob
+import os
+from pandas import DataFrame, read_csv
+import pandas as pd
+import numpy as np
+from collections import Counter
+import itertools
+
+
+filelist = glob('*.list')
+l = len(filelist)
+
+d = []
+for file in filelist:
+	print('\n' + file)
+	df = pd.read_csv(file, index_col =False, sep='\t', header=(0), )
+	a = list(df.loc[(df['Reads']>= 30) & (df['Mismatch %']>0) & (df['Mismatch %']<2), 'Contig'])
+	d.append(a)
+
+d = list(itertools.chain.from_iterable(d))
+
+dict_list = (Counter(d))
+for key, value in dict_list.items():
+    if value == l:
+        print key
+
+	
