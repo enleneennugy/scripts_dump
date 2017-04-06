e1a8ad839b1e5095faa1561b215e374280dac546
vincent manzanilla <vincent.manzanilla@nhm.uio.no>
2017-04-06 09:57:16 +0100

    cosmetic

--- fetch_seq_from genbank/fetch_seq_from_genbank.py
+++ fetch_seq_from genbank/fetch_seq_from_genbank.py
@@ -1,31 +1,68 @@
 #!/usr/bin/env python2
 
-##Dependencies: Biopython
-# to install biopython copy paste in your terminal: sudo pip install biopython
+# Download sequences from genbank according a list of genera or families or species. 
 
-## Output:
-# individual file per genera or family or species from the input list
-# if your asking thousands of sequences to genbank, you might concider to divide your list into smaller ones.
 
-##Usage
-# python fetch_seq_from_genbank.py
-# note: need to have the list file in the same folder.
+# - Dependencies: Biopython
+#     To install biopython copy paste in your terminal: 
+#     ```
+#      sudo pip install biopython
+#     ```
 
-##list file example: one query per line
-#Aulosepalum
-#Beloglottis
-#Brachystele
-#Coccineorchis
-#Cotylolabium
-#Cyclopogon
-#Deiregyne
-#Dichromanthus
-#Dithyridanthus
+# - List file example: one query per line
+#     ```
+#     Aulosepalum
+#     Beloglottis
+#     Brachystele
+#     Coccineorchis
+#     Cotylolabium
+#     Cyclopogon
+#     Deiregyne
+#     Dichromanthus
+#     Dithyridanthus
+#     ```
+# - Usage:
+#     note: need to have the list file in the same folder.
+#     ```
+#     python fetch_seq_from_genbank.py
+#     ```
 
-##Fasta output
-# if you want some fasta as output file:
-# CHANGE line 47: f = open(genus+".gb", 'w') ###change ".gb" for ".fas"
-# CHANGE line 52: SeqIO.write(record, f, "gb") ###change "gb" for "fasta"
+# - Output:
+#     - individual file per genera or family or species from the input list
+#     - if your asking thousands of sequences to genbank, you might concider to divide your list into smaller ones.
+#     Example of terminal output: 
+#     ```
+#     genus    number_of_seq   sequence_ID     length
+#     Aulosepalum:    1
+#                 AJ542433    1316
+#     Beloglottis:    1
+#                 AJ542432    1314
+#     Brachystele:    3
+#                 FJ571317    1331
+#                 FN870776    1341
+#                 JQ045462    699
+#     Coccineorchis:  1
+#                 AJ542422    1316
+#     Cotylolabium:   0
+#     Cyclopogon:     3
+#                 GQ916978    834
+#                 FJ571323    1331
+#                 AJ542425    1316
+#     Deiregyne:  1
+#                 AJ542440    1316
+#     Dichromanthus:  2
+#                 AJ542439    1316
+#                 AJ542438    1314
+#     ```
+
+- Fasta output
+    If you want a fasta formated file as output file: 
+    ```
+    CHANGE line 84: f = open(genus+".gb", 'w') ###change ".gb" for ".fas"
+    CHANGE line 89: SeqIO.write(record, f, "gb") ###change "gb" for "fasta"
+    ```
+
+
 
 from Bio import Entrez, SeqIO
 
