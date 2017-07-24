c42aced8733efa362fc0f5fac5a55fbf732b895d
vincent manzanilla <vincent.manzanilla@nhm.uio.no>
2017-07-24 08:48:05 +0200

    export df matrix for heatmap

--- add_missing_sample_to_matrices/counting_number_of_missing_data_accross_matrices.py
+++ add_missing_sample_to_matrices/counting_number_of_missing_data_accross_matrices.py
@@ -4,32 +4,36 @@ from Bio import SeqIO
 from Bio.SeqRecord import SeqRecord
 from Bio.Seq import Seq
 import os
-from collections import Counter
-import collections
-import itertools
+
+import pandas as pd
 
 
 z = {}
+df = pd.DataFrame()
 
-listfile = glob.glob("*fas")
-list_sample = open("list", "r")
-list_sample = list_sample.read().splitlines()
-a=len(listfile)
+listfile = glob.glob("*fasta")
+
+a = len(listfile)
+
+
 for matrix in listfile:
 	d = {}
+	indexd = matrix.split('.')[0]
 	for record in SeqIO.parse(matrix, 'fasta'):
 		length = len(record.seq)
 		recordSEQ = record.seq
 		recordid = record.id.split('.')[0]
-		nCount = recordSEQ.lower().count('-') 
+		nCount = recordSEQ.lower().count('-')
 		ratio = ((nCount/(length*1.00)))
 		
 		d[recordid] = ratio	
+	df1 = pd.DataFrame([d], index=[indexd])
+	df = pd.concat([df,df1], axis=0)
 	z = {x: d.get(x, 0) + z.get(x, 0) for x in set(d).union(z)}
 
-
+df.to_csv("missing_data.csv", sep='\t')
 print "percentage of missing data per sample"
 for key, value in z.items():
-    z[key] = float(format(value/a, '.4f'))
+	z[key] = float(format(value/a, '.4f'))
 for kv in (sorted(z.items(), key=lambda x:x[1])):
 	print kv[0],'\t',kv[1]
